87ce8e900a90f9b248b53407422356ccbcfca51f
shutterberg <49706189+shutterberg@users.noreply.github.com>
2021-06-23 13:30:19 +0530

    Update 275A.cpp
    A. Word Capitalization
    time limit per test
    2 seconds
    memory limit per test
    256 megabytes
    input
    standard input
    output
    standard output
    
    Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
    
    Note, that during capitalization all the letters except the first one remains unchanged.
    Input
    
    A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
    Output
    
    Output the given word after capitalization.

--- 275A.cpp
+++ 275A.cpp
@@ -4,7 +4,6 @@
 #define init  ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define fori(i, a, b) for (ll i = a; i < (ll)b; i++)
 using namespace std;
-const ll mod = 1e9 + 7;
 int main()
 {
     int arr[3][3], i, j, op[3][3];
