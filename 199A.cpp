bed3499b3eed63c8ccc3eb0a53edf39a09caa363
shutterberg <misquithin@gmail.com>
2021-06-25 22:56:22 +0530

    Create 199A.cpp
    A. Hexadecimal's theorem
    time limit per test
    2 seconds
    memory limit per test
    256 megabytes
    input
    standard input
    output
    standard output
    
    Recently, a chaotic virus Hexadecimal advanced a new theorem which will shake the Universe. She thinks that each Fibonacci number can be represented as sum of three not necessary different Fibonacci numbers.
    
    Let's remember how Fibonacci numbers can be calculated. F0 = 0, F1 = 1, and all the next numbers are Fi = Fi - 2 + Fi - 1.
    
    So, Fibonacci numbers make a sequence of numbers: 0, 1, 1, 2, 3, 5, 8, 13, ...
    
    If you haven't run away from the PC in fear, you have to help the virus. Your task is to divide given Fibonacci number n by three not necessary different Fibonacci numbers or say that it is impossible.
    Input
    
    The input contains of a single integer n (0 ≤ n < 109) — the number that should be represented by the rules described above. It is guaranteed that n is a Fibonacci number.
    Output
    
    Output three required numbers: a, b and c. If there is no answer for the test you have to print "I'm too stupid to solve this problem" without the quotes.
    
    If there are multiple answers, print any of them.

---
+++ 199A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define init ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fori(i,a,b) for(ll i=a;i<=(ll)b;i++)
+using namespace std;
+const ll mod =1e9+7;
+int main()
+{
+    init; 
+    int n;
+    cin>>n;
+    cout<<"0 0 "<<n;
+    return 0;
+}
