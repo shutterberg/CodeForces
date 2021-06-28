0c7d3c29eef60166c18aca353e63aab1178e22d3
shutterberg <misquithin@gmail.com>
2021-06-28 20:01:11 +0530

    Create 124A.cpp
    A. The number of positions
    time limit per test
    0.5 second
    memory limit per test
    256 megabytes
    input
    standard input
    output
    standard output
    
    Petr stands in line of n people, but he doesn't know exactly which position he occupies. He can say that there are no less than a people standing in front of him and no more than b people standing behind him. Find the number of different positions Petr can occupy.
    Input
    
    The only line contains three integers n, a and b (0 ≤ a, b < n ≤ 100).
    Output
    
    Print the single number — the number of the sought positions.

---
+++ 124A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define init                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define fori(i, a, b) for (ll i = a; i <= (ll)b; i++)
+using namespace std;
+int main()
+{
+    init;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+	int n,f,b;
+    cin>>n>>f>>b;
+    cout<<min(n-f,b+1);
+
+	return 0;
+}
